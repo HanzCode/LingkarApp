5cbd4bb725450ab2f4c2fc51458d475398115636
Farhan <farhan03com@gmail.com>
2024-01-12 23:11:07 +0800

    first commit

---
+++ LingkarApps/app/src/main/java/com/example/lingkarapps/TskSelfAnalysisActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lingkarapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TskSelfAnalysisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tsk_self_analysis)
+    }
+}
